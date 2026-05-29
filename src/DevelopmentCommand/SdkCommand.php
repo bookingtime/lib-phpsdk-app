1fb34ccd6466d92201562b98a02a76106ba5675d
Cathrin Bott <c.bott@bookingtime.com>
2026-05-29 15:48:28 +0200

    wip: new static route for adminnews

--- src/DevelopmentCommand/SdkCommand.php
+++ src/DevelopmentCommand/SdkCommand.php
@@ -1003,6 +1003,10 @@ class SdkCommand extends Command {
 		// $sectorList=$sdk->static_sector_list([]);
 		// $timeZoneList=$sdk->static_timeZone_list([]);
 			#$organizationTemplateDataList=$sdk->static_organizationTemplateData_list([]);
+$adminNewsList=$sdk->static_adminNews_list([]);
+
+die(BasicLib::debug($adminNewsList));
+
 		// $error=$sdk->static_error_show(['errorCode'=>400]);
 		// $error=$sdk->static_error_show(['errorCode'=>401]);
 		// $error=$sdk->static_error_show(['errorCode'=>403]);
